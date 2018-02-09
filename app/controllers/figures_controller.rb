1d71753a80c614bc565c87eb190417f16a0dbbf1
Terry-Thompson <terry.lyn.thompson@gmail.com>
2018-02-09 14:00:49 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,14 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do 
+		erb :'/figures/index'
+	end
+
+
 	get '/figures/new' do
-		binding.pry
+		@titles = Title.all
+		@landmarks = Landmark.all
 		erb :'/figures/new'
 	end
+
 end
