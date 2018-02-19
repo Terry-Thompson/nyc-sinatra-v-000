50f0caddbe1ae26d9211a9e71d28e8f03c2fef67
Terry-Thompson <terry.lyn.thompson@gmail.com>
2018-02-19 16:14:52 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,4 +11,37 @@ class FiguresController < ApplicationController
 		erb :'/figures/new'
 	end
 
-end
+	get '/figures/:id' do 
+		@figure = Figure.find(params[:id])
+		erb :'/figures/show'
+	end
+
+	get '/figures/:id/edit' do 
+		@figure = Figure.find(params[:id])
+		erb :'/figures/edit'
+	end
+
+
+	post '/figures' do
+		@figure = Figure.find_or_create_by(name: params[:figure][:name])
+		title_ids = params[:figure][:title_ids]
+		landmark_ids = params[:figure][:landmark_ids]
+
+		if title_ids
+			title_ids.each {|id| @figure.titles << Title.find(id)}
+		elsif landmark_ids 
+			landmark_ids.each {|id| @figure.landmarks << Landmark.find(id)}
+		elsif !params[:title][:name].empty? 
+			@figure.titles << Title.find_or_create_by(name: params[:title][:name])  
+		elsif !params[:landmark][:name].empty? 
+			@landmark = Landmark.find_or_create_by(name: params[:landmark][:name])  
+			@figure.landmarks << @landmark 
+			@landmark.save
+		end# of if 
+
+		@figure.save		
+		erb :'/figures/show'
+	end# of post '/figures'
+
+
+end# of class
